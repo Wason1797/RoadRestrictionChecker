365577b7c58f834e53453cb1d9ffd9a8b61b3c0d
Wladymir Brborich <wabrborich@hotmail.com>
2020-05-27 01:24:49 -0500

    add main program logic

---
+++ checker/__init__.py
@@ -0,0 +1,1 @@
+from checker.road_restriction_checker import RoadRestrictionChecker
---
+++ main.py
@@ -0,0 +1,22 @@
+from utils import parser
+from models.plate import Plate
+from checker import RoadRestrictionChecker
+from config.restrictions import GeneralRuleset
+from errors.validation_errors import PlateValidationError
+
+
+if __name__ == "__main__":
+    args = parser.configure_argument_parser().parse_args()
+    time = parser.to_time(args.time)
+    week_day = parser.to_date(args.date).isoweekday()
+    plate = Plate(args.plate)
+
+    if not plate.is_valid:
+        raise PlateValidationError(f"Plate {plate} does not match the allowed standard")
+
+    print(f"{plate} belongs to a {plate.plate_type}")
+
+    if RoadRestrictionChecker(GeneralRuleset).is_car_restricted(plate, week_day, time):
+        print(f"You have road restriction at the moment")
+    else:
+        print("You don't have road restriction at the moment")
