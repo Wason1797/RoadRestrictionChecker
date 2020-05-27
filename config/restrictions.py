49f72a5987a11553f21330976c188a2730de04ba
Wladymir Brborich <wabrborich@hotmail.com>
2020-05-27 01:24:32 -0500

    bug fixes with super and set()

--- config/restrictions.py
+++ config/restrictions.py
@@ -10,10 +10,11 @@ class RuleSet:
         3: {5, 6},
         4: {7, 8},
         5: {9, 0},
-        6: {},
-        7: {},
+        6: set(),
+        7: set(),
     }
 
+    ALLOW_SPECIAL_CASES = True
     APPLY_TO = {VehicleTypes.COMERCIAL_VEHICLE, VehicleTypes.PRIVATE_VEHICLE}
     TIME_WINDOWS = None
 
@@ -32,6 +33,7 @@ class ExtendedRuleset(RuleSet):
 class LockdownRuleset(RuleSet):
 
     WEEK_DAY_RESTRICTIONS = {week_day: {1, 2, 3, 4, 5, 6, 7, 8, 9, 0} - digits
-                             for week_day, digits in super().WEEK_DAY_RESTRICTIONS.items()}
+                             for week_day, digits in RuleSet.WEEK_DAY_RESTRICTIONS.items()}
 
-    TIME_WINDOWS = ((to_time('14:00'), to_time('5:00')), )
+    TIME_WINDOWS = ((to_time('00:00'), to_time('5:00')),
+                    (to_time('14:00'), to_time('23:59')))
